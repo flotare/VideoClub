c35423e4805b8bb897ff6d125e8fc3d576380228
ArthurWav <arthur.wavreille@etu.mines-ales.fr>
2025-11-12 12:46:46 +0100

    Added controller for simple video information page

---
+++ workspace-spring/VideoClub/src/main/java/CinephileConfrerie/VideoClub/Control/VideoControl.java
@@ -0,0 +1,23 @@
+package CinephileConfrerie.VideoClub.Control;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import CinephileConfrerie.VideoClub.model.Media.Video;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+/**
+ * Controller that manages the individual pages of the videos
+ */
+@RestController
+public class VideoControl {
+
+    @RequestMapping(value = "/video/{id}", method=RequestMethod.GET)
+    public Video requestMethodName(@PathVariable Video video) {
+        return new Video();
+    }
+    
+}
