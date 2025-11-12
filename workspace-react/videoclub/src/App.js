dc9623ee943a20af28db772cf61d2f200991395a
ArthurWav <arthur.wavreille@etu.mines-ales.fr>
2025-11-12 12:08:00 +0100

    Added the first react component and main page app

--- workspace-react/videoclub/src/App.js
+++ workspace-react/videoclub/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import ListeFilm from './ListeFilm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li> <a href=".">Link1</a> </li>
+              <li> <a href=".">Link2</a> </li>
+              <li> <a href=".">Link3</a> </li>
+            </ul>
+          </nav>
+        </header>
+
+        <main class="App-body">
+          <ListeFilm></ListeFilm>
+        </main>
+        
+      </div>
+    </>
   );
 }
 
---
+++ workspace-react/videoclub/src/ListeFilm.js
@@ -0,0 +1,21 @@
+import './ListeFilm.css';
+
+export default function ListeFilm() {
+
+
+    return(
+    <div>
+        <div class="App-body-image">
+            <img src="https://via.placeholder.com/300x200" alt="Img 1"></img>
+            <p>Description de l’image 1</p>
+        </div>
+        <div class="App-body-image">
+            <img src="https://via.placeholder.com/300x200" alt="Img 2"></img>
+            <p>Description de l’image 2</p>
+        </div>
+        <div class="App-body-image">
+            <img src="https://via.placeholder.com/300x200" alt="Img 3"></img>
+            <p>Description de l’image 3</p>
+        </div>
+    </div>)
+}
